37605a44254a8bc0bc61221ee2ac8b16e1b4cc50
Pavol Havlik <pavolhavlik28@gmail.com>
2023-10-23 19:38:47 +0200

    Added whitelist to global ValidationPipe to trim unwanted paremeters from dtos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true // trimming unwanted properties sent to dto
+    }))
     await app.listen(3333);
 }
 bootstrap();
